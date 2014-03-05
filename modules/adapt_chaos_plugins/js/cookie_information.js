0177d0ebfaf2949dd8d6edaef0c354db9ba89264
Tom Helmer Hansen <thelmer@thehome.dk>
2014-03-05 18:21:30 +0100

    Add the resuable chaos plugin module

---
+++ modules/adapt_chaos_plugins/js/cookie_information.js
@@ -0,0 +1,32 @@
+/**
+ * Implements basic handling of cookie information rendering
+ */
+(function($) {
+  Drupal.behaviors.cookieInformationHandler = {
+    attach: function(context, settings) {
+      var $cookiePopup = $('.js-cookie-information-popup', context);
+      if ($cookiePopup.length > 0) {
+          // If we do not have a cookie show the message and set the cookie.
+          if ($.cookie('cookieInformationPresented') != 1) {
+            // Add a close button!
+            var closeButton = $('<div class="close">' + Drupal.t('OK', {}, {context: "Cookie information"}) + '</div>');
+            closeButton.bind('click', function () {
+              $cookiePopup.fadeOut(600);
+            });
+            $cookiePopup.find('.cookie-information-container').append(closeButton);
+
+            // Insert the cookie information pane at the top of the page.
+            $('body').prepend($cookiePopup);
+            $cookiePopup.fadeIn(600);
+
+            // Set cookie
+            $.cookie('cookieInformationPresented', 1, { expires: 365, path: '/' });
+          }
+          else {
+            // If we have a cookie, do not show the message.
+          }
+      }
+    }
+  }
+})(jQuery);
+
---
+++ modules/adapt_chaos_plugins/tpl/cookie-information.tpl.php
@@ -0,0 +1,5 @@
+<div class="js-cookie-information-popup">
+  <div class="cookie-information-container">
+    <?php print $message; ?>
+  </div>
+</div>
